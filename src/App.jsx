11076997fa6169dd25cdce6e41bb04d39ef53edf
Essam <esamanwar2@gmail.com>
2025-06-28 16:46:22 +0300

    [+] add NavbarComponent and its route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Invoice from "./components/Invoice";
 import './App.css';
+import NavbarComponent from "./components/navbar";
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Invoice />} />
-        <Route path="/" element={<Invoice />} />
+        <Route path="/" element={<NavbarComponent />} />
+        <Route path="/invoice" element={<Invoice />} />
       </Routes>
     </Router>
   );
