dbd178b4b3985a3050f4167586293c97c3d25abe
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-01 16:50:07 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Invoice from "./components/Invoice";
 import './App.css';
-import NavbarComponent from "./components/navbar";
-
+import NavbarComponent from "./components/Navbar";
+import Preview from "./components/Preview";
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<NavbarComponent />} />
         <Route path="/invoice" element={<Invoice />} />
+        <Route path="/preview" element={<Preview />} />
+
       </Routes>
     </Router>
   );
