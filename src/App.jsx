b5e452ff2b25be52bfe5b28c2227dbff1288b562
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-06 17:18:02 +0300

    Modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Invoice from "./components/Invoice";
-import './App.css';
-import NavbarComponent from "./components/Navbar";
-import Preview from "./components/Preview";
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<NavbarComponent />} />
-        <Route path="/invoice" element={<Invoice />} />
-        <Route path="/preview" element={<Preview />} />
+// src/App.jsx
 
-      </Routes>
-    </Router>
-  );
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Invoice from "./components/Invoice";
+import Preview from "./components/Preview";
+import NavbarComponent from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+          
+<Routes>
+    {/* <Route path="/" element={<Login />} /> */}
+        <Route index element={<Login />} />   
+    <Route path="/signup" element={<Signup />} />
+  <Route path="/" element={<Login />} />
+<Route path="/home" element={<NavbarComponent />} />
+
+    <Route path="/invoice" element={<Invoice />} />
+    <Route path="/preview" element={<Preview />} />
+</Routes>
+
+        </Router>
+    );
 }
 
 export default App;
